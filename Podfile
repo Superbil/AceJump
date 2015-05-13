c1261db1a3204a26c4c7c721089271d7167b5b2a
Superbil <superbil@gmail.com>
2015-05-13 14:57:41 +0800

    Init cocoapods and pods
    - add .gitignore
    - install pod DTXcodeUtils
    - install podMASShortcut

---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :osx
+
+target 'AceJump' do
+  pod 'DTXcodeUtils', '~> 0.1'
+  pod 'MASShortcut', '~> 2.1'
+end
---
+++ Pods/Target Support Files/Pods-AceJump/Pods-AceJump-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// DTXcodeUtils
+#define COCOAPODS_POD_AVAILABLE_DTXcodeUtils
+#define COCOAPODS_VERSION_MAJOR_DTXcodeUtils 0
+#define COCOAPODS_VERSION_MINOR_DTXcodeUtils 1
+#define COCOAPODS_VERSION_PATCH_DTXcodeUtils 1
+
+// MASShortcut
+#define COCOAPODS_POD_AVAILABLE_MASShortcut
+#define COCOAPODS_VERSION_MAJOR_MASShortcut 2
+#define COCOAPODS_VERSION_MINOR_MASShortcut 1
+#define COCOAPODS_VERSION_PATCH_MASShortcut 2
+
